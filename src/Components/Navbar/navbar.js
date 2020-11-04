1dd5b684246664534085b05cae10660bf834de41
EASYMAK777 <jplocallawncare@gmail.com>
2020-11-04 03:53:32 -0500

    turned logo into link that refreshes page

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "../Navbar/Navbar.css"
 
 function Navbar() {
     return (
         <nav className="header">
             {/* logo on the left -> */}
+            <Link to="/">
             <img className="header___logo" 
             src = "http://pngimg.com/uploads/amazon/amazon_PNG11.png"
             alt = "amazon logo"
             />
+            </Link>
+            
 
 
             {/* Search box */}
