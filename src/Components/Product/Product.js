b1ea856b20cd8fe65ea8ccf2e915561e0f5d5f23
EASYMAK777 <jplocallawncare@gmail.com>
2020-11-05 18:13:31 -0500

    created an array for product  rating added start icons into an array,created price props, title props, and rating props

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -5,6 +5,18 @@ function Product({ id, title, image, price, rating }) {
     return (
         <div className="product">
             <p>{title}</p>
+            <p className="product__">
+                <small>$</small>
+                <strong>{price}</strong>
+                <div className="product__rating">
+                    {
+                        Array(rating)
+                        .fill()
+                        .map((_) => (
+                            <p>⭐</p>
+                        ))}
+                </div>
+            </p>
         </div>
     )
 }
