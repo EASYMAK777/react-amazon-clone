ceba6cfcff033fbfc6cd61e81516e20d7ae392b8
EASYMAK777 <jplocallawncare@gmail.com>
2020-11-12 17:40:35 -0500

    importing reducer comp to index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StateProvider } from './Components/StateProvider/StateProvider';
-import {initialState} from './Components/Reducer/Reducer'
+import reducer, {initialState} from './Components/Reducer/Reducer'
+
 
 ReactDOM.render(
   
   <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={}>
+    <StateProvider initialState={initialState} reducer={reducer}>
       <App />
     </StateProvider>
   </React.StrictMode>,
