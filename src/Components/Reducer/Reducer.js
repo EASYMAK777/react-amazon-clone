c2cce13676cd3077a54774124e22bfddfc1ef8d9
EASYMAK777 <jplocallawncare@gmail.com>
2020-11-12 17:38:24 -0500

    created logic for add to and removing from basket using switch case

--- src/Components/Reducer/Reducer.js
+++ src/Components/Reducer/Reducer.js
@@ -3,4 +3,15 @@ export const initialState = {
     // user:null
 };
 
-function reducer(state, action)
+function reducer(state, action) {
+    switch(action.type) {
+     case 'ADD_TO_BASKET':
+        //Logic for adding item to basket 
+        break;
+     case 'REMOVE_FROM_BASKET':
+        //  Logic for removing item from basket
+        break;
+    default:
+        return state;
+    }
+}
