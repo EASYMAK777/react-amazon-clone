3234914a8f8a1155845de8c63dcbbcf2732d366e
EASYMAK777 <jplocallawncare@gmail.com>
2020-11-22 12:31:41 -0500

    added img

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -5,8 +5,10 @@ function Checkout() {
     const [{ basket }] = useStateValue();
     return (
         <div className="checkout">
-           <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/
-           images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423494660_.jpg" alt="image">
+           <img className="checkout__ad" 
+           src="https://images-na.ssl-images-amazon.com/
+           images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423494660_.jpg" 
+           alt="image">
           </img> 
         </div>
     )
