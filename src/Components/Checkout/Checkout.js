561c746ca5a1ab57bd11faa35d72f67a710d2f3b
EASYMAK777 <jplocallawncare@gmail.com>
2020-11-22 12:55:37 -0500

    added headers and class name for checkout title, imported use state value for basket fucntion

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import Image200 from '../img/Amazon.ad.jpg'
 import './Checkout.css'
+import useStateValue from '../Reducer/Reducer'
 
 function Checkout() {
 
-    // const [{ basket }] = useStateValue();
+    const [{ basket }] = useStateValue();
     return (
         <div className="checkout">
            <img className="checkout__ad" 
            src={Image200} 
            alt="image"
            />
-           <h1>Test</h1>
+           {basket?.length === 0 ? (
+             <div>
+                 <h2>Your Shopping Basket is empty</h2>
+                 <p>
+                     You have no items in your basket.  To buy one or more items, click
+                    "Add to basket" next to the item.
+                 </p>
+             </div>
+           ) : (
+               <div>
+                   <h2 className="checkout__title">Your Shopping Basket</h2>
+               </div>
+           )}
            
         </div>
     )
