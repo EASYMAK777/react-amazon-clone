4ee1f28d2967086bf12787d17294236314c1d6a0
EASYMAK777 <jplocallawncare@gmail.com>
2021-02-17 13:08:02 -0500

    created login className, imported for logo

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Login.css'
+import logo from "../img/blistlyfe-logo.jpg"
+import { Link } from "react-router-dom"
 
 function Login() {
     return (
         <div className="login">
+            <Link too ='/'>
+                <img
+                className="login__logo"
+                src={ logo }
+                />
+            </Link>
             <h1>Login Page</h1>
         </div>
     )
