1dbad37d8fbe2354ffd89e8c456e61f9f2218a77
EASYMAK777 <jplocallawncare@gmail.com>
2021-02-23 12:55:21 -0500

    useEffect function to track if the user is logged in or out

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css';
 import Navbar from "./Components/Navbar/navbar"
 import Home from './Components/Home/Home'
 import Login from './Components/Login/Login'
 import Checkout from './Components/Checkout/Checkout'
+import { auth } from "./Components/Firebase/firebase"
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
+
+  useEffect(() => {
+      // will only run once when the app component loads...
+      auth.onAuthStateChanged(authUser => {
+        console.log('THE USER IS ...', authUser);
+      } )
+  }, [])
+
   return (
     <Router>
       <div className="App">
