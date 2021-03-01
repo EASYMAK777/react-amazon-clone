344a259ab5726807aa6a4afbf712bee29e484786
EASYMAK777 <jplocallawncare@gmail.com>
2021-03-01 17:24:37 -0500

    added payment address class

--- src/Components/Payment/Payment.js
+++ src/Components/Payment/Payment.js
@@ -4,22 +4,26 @@ import Navbar from "../Navbar/navbar"
 
 function Payment(){
     return(
-        <div className = "Payment">
+        <div className = "payment">
           <div className="payment__container">
               {/* Payment Section - deliver addres */}
-            <div className = "Payment__section">
+            <div className = "payment__section">
+                <h3>Delivery Address</h3>
+            </div>
+
+            <div className="payment__address">
 
             </div>
 
               
               {/* Payment Section - Review Items */}
-            <div className = "Payment__section">
-
+            <div className = "payment__section">
+                <h3>Review Your Items</h3>
             </div>
 
               {/* Payment Method  */}
-            <div className = "Payment__section">
-                
+            <div className = "payment__section">
+               <h3>Payment Method</h3> 
             </div>  
           
           
