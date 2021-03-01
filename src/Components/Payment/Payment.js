d5865fbeea8a1234771a36ea67b91274035877b0
EASYMAK777 <jplocallawncare@gmail.com>
2021-03-01 17:21:48 -0500

    created payment container and 3 payment section classes for styling

--- src/Components/Payment/Payment.js
+++ src/Components/Payment/Payment.js
@@ -5,8 +5,26 @@ import Navbar from "../Navbar/navbar"
 function Payment(){
     return(
         <div className = "Payment">
+          <div className="payment__container">
+              {/* Payment Section - deliver addres */}
+            <div className = "Payment__section">
+
+            </div>
+
+              
+              {/* Payment Section - Review Items */}
+            <div className = "Payment__section">
+
+            </div>
+
+              {/* Payment Method  */}
+            <div className = "Payment__section">
+                
+            </div>  
+          
           
 
+          </div>
         </div>
     )
 }
